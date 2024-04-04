b9377eb288338e7fe442cc38e4e5ce999a94b67f
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-04-04 18:08:48 -0300

    feat: ends calculator of simple interest

--- main.c
+++ main.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void simpleInterest(void);
+void compoundInterest(void);
+
 int main(void)
 {
-  
+
+  int operation;
+
+  printf("This calculator determines the final amount. \n");
+  printf("Type 1 for 'simple interest' or type 2 for 'compound interest': ");
+
+  do
+  {
+    scanf("%i", &operation);
+  } while (operation != 1 && operation != 2);
+
+  printf("The interest rate and time must refer to the same unit of time. \n");
+  printf("Use a period ( '.' ) to separate decimal places.");
+
+  (operation == 1) ? simpleInterest() : compoundInterest();
+
   return 0;
 }
+
+void simpleInterest(void)
+{
+  float result;
+
+  float initialValue;
+  float interestRate;
+  float time;
+
+  printf("Type the initial value: ");
+  scanf("%f", &initialValue);
+
+  printf("Type the interest rate ( write in decimal ): ");
+  scanf("%f", &interestRate);
+
+  printf("Type the time: ");
+  scanf("%f", &time);
+
+  result = initialValue + (initialValue * interestRate * time);
+  printf("final amount: %f", result);
+}
+
+void compoundInterest(void) {}
